cc5cb8e49ab403853b8a733ccffc796e324d9789
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2023-11-21 10:20:00 +0200

    Update screen.lua

--- 3DEngine/screen.lua
+++ 3DEngine/screen.lua
@@ -1,12 +1,13 @@
 if ( not Inited ) then require( "init" ) return end
 
-local w, h, hw, hh, ratio
+local w, h, hw, hh, ratio, ratio2
 
 function showscreen()
     draw.showdrawscreen()
     w, h = draw.getdrawsize()
     hw, hh = w  * .5, h * .5
     ratio  = w / h
+    ratio2  = h / w
 
     exec( "screen" )
 end
@@ -37,4 +38,8 @@ end
 
 function ScrRatio()
     return ratio
+end
+
+function ScrRatio2()
+    return ratio2
 end
