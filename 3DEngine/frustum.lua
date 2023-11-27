15ba8340ce4e0f2cc79d533f5403bc0ec45ceeaa
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2023-11-27 13:07:58 +0200

    Update frustum.lua

--- 3DEngine/frustum.lua
+++ 3DEngine/frustum.lua
@@ -9,19 +9,50 @@ local _FRUSTUM = {
 
     far = nil,
     near = nil,
+
+    fz = nil,
+    nz = nil,
+    fov = nil,
+    aspect = nil,
 }
 
+function updateFrustum()
+    vec3set( _FRUSTUM.near[1], GetCamDir() )
+    vec3mul( _FRUSTUM.near[1], _FRUSTUM.zn )
+    vec3add( _FRUSTUM.near[1], GetCamPos() )
+
+    vec3set( _FRUSTUM.near[2], GetCamDir() )
+
+
+    vec3set( _FRUSTUM.far[1], GetCamDir() )
+    vec3mul( _FRUSTUM.far[1], _FRUSTUM.zf )
+    vec3add( _FRUSTUM.far[1], GetCamPos() )
+
+    vec3set( _FRUSTUM.far[2], GetCamDir() )
+    vec3mul( _FRUSTUM.far[2], -1 )
+end
+
 function buildFrustum( fov, aspect, zn, zf )
+    _FRUSTUM.zf = zf
+    _FRUSTUM.zn = zn
+    _FRUSTUM.fov = fov
+    _FRUSTUM.aspect = aspect
+
     local halfVSide = zf * math.tan(fov * .5)
     local halfHSide = halfVSide * aspect
     local frontMultFar = vec3mul( vec3( GetCamDir() ), zf )
 
+    _FRUSTUM.hvside = halfVSide
+    _FRUSTUM.hhside = halfHSide
+    _FRUSTUM.frontMulFar = frontMultFar
+
     _FRUSTUM.near = {
-        vec3mul( vec3add( vec3( GetCamPos() ), zn ), GetCamDir() ),
+        vec3add( vec3mul( vec3( GetCamDir() ), zn ), GetCamPos() ),
         vec3( GetCamDir() )
     }
+
     _FRUSTUM.far  = {
-        vec3add( vec3( GetCamPos() ), frontMultFar ),
+        vec3add( vec3mul( vec3( GetCamDir() ), zf ), GetCamPos() ),
         vec3negate( vec3( GetCamDir() ) )
     }
 
