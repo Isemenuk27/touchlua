9b6f93e80af6678f80cc4ee4ba8d753db4f0bc12
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-01-12 13:21:13 +0200

    Update aabb.lua

--- 3DEngine/scenes/aabb.lua
+++ 3DEngine/scenes/aabb.lua
@@ -1,3 +1,12 @@
+if ( not Inited ) then
+    local path = debug.getinfo(1, "S").source:match("@(.*)")
+    for i = #path, 1, -1 do; local c = string.sub( path, i, i );
+    if ( c == "/" or c == "\\" ) then return string.sub( path, i + 1 ) end end
+    _SCENETOLOAD = string.sub( path, 1, -5)
+    package.path = package.path .. ";../../?.lua"
+    return require("3DEngine/init")
+end
+
 local obj = createclass( C_POLY )
 obj:born()
 obj.form = loadobj( "f16.obj" )
