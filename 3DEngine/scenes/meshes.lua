3fb1f365e578867526d91d2a59b85b1853c5a31c
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-01-12 13:22:09 +0200

    Update meshes.lua

--- 3DEngine/scenes/meshes.lua
+++ 3DEngine/scenes/meshes.lua
@@ -1,3 +1,12 @@
+if ( not Inited ) then
+    local path = debug.getinfo(1, "S").source:match("@(.*)")
+    for i = #path, 1, -1 do; local c = string.sub( path, i, i );
+    if ( c == "/" or c == "\\" ) then return string.sub( path, i + 1 ) end end
+    _SCENETOLOAD = string.sub( path, 1, -5)
+    package.path = package.path .. ";../../?.lua"
+    return require("3DEngine/init")
+end
+
 local meshes = {
     loadobj( "osprey.obj" ),
     loadobj( "f16.obj" ),
@@ -35,7 +44,7 @@ local function Loop( CT, DT )
         obj.form = meshes[meshid]
     end
 
-    vec3set( obj.ang, 0, math.pi * CT, 0 )
+    vec3set( obj.ang, 0, math.pi * CT * .2, 0 )
     vec3set( lamp.pos, math.cos( CT ) * 17, math.sin( CT ) * 17, 0 )
 end
 
