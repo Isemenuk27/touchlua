9495393000c2b91bac0472d3c5728ff8ee8bb245
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-01-12 13:21:50 +0200

    Update cube.lua

--- 3DEngine/scenes/cube.lua
+++ 3DEngine/scenes/cube.lua
@@ -1,7 +1,16 @@
+if ( not Inited ) then
+    local path = debug.getinfo(1, "S").source:match("@(.*)")
+    for i = #path, 1, -1 do; local c = string.sub( path, i, i );
+    if ( c == "/" or c == "\\" ) then return string.sub( path, i + 1 ) end end
+    _SCENETOLOAD = string.sub( path, 1, -5)
+    package.path = package.path .. ";../../?.lua"
+    return require("3DEngine/init")
+end
+
 local obj = createclass( C_POLY )
 obj:born()
 obj.form = loadobj( "cube.obj" )
-obj.scl = vec3( 1 )
+obj.scl = vec3( .5 )
 obj.ang = vec3( 0, 0, 0 )
 obj.solid = true
 
@@ -16,10 +25,13 @@ lamp:setPos( vec3( 0, 2, 4 ) )
 lamp:setDir( vec3normalize( vec3( -0.015374, 0.928981, -0.369808 ) ) )
 lamp.diffuse = vec3mul( vec3( 255, 0, 0 ), 1 / 255 )
 
+local out = {}
 local function Loop( CT, DT )
     vec3set( lamp.pos, math.cos( CT ) * 17, math.sin( CT ) * 17, 0 )
 
-    vec3add( obj.ang, 0, .25 * DT * math.pi, 0 )
+    --vec3add( obj.ang, 0, .25 * DT * math.pi, 0 )
+
+    traceRay( GetCamPos(), GetCamDir(), 6, out )
 end
 
 callback( _LOOPCALLBACK, Loop )
