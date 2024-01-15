2ea7148f7802da965ecdc58a7ce4c688e26b13d0
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-01-15 10:03:42 +0200

    Create screen.lua

---
+++ 2DEngine/screen.lua
@@ -0,0 +1,41 @@
+if ( not Inited ) then require( "init" ) return end
+
+local w, h, hw, hh, ratio
+
+function showscreen()
+    draw.showdrawscreen()
+    w, h = draw.getdrawsize()
+    hw, hh = w  * .5, h * .5
+    ratio  = h / w
+
+    exec( "screen" )
+end
+
+function ScrW()
+    return w
+end
+
+function ScrH()
+    return h
+end
+
+function HScrW()
+    return hw
+end
+
+function HScrH()
+    return hw
+end
+
+function Scr()
+    return w, h
+end
+
+function HScr()
+    return hw, hh
+end
+
+function ScrRatio()
+    return
+	ratio
+end
