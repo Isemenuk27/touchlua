a3a091cfa13e240b25ec2c5a5559bfd3bdd75b01
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-01-15 10:02:35 +0200

    Create menu.lua

---
+++ 2DEngine/menu.lua
@@ -0,0 +1,43 @@
+if ( not Inited ) then require( "init" ) return end
+
+local btns = {}
+
+local function killbuttons()
+    for i, b in ipairs( btns ) do
+        GUI.KillElement( b )
+    end
+end
+
+
+local w, h = Scr()
+local hw, hh = HScr()
+
+local function menuLoop( DT )
+    text( round( 1 / DT, 2 ), 20, 20, red )
+end
+
+local function menucreate( w, h )
+    for _, f in ipairs( sys.dir( "scenes" ) ) do
+        if ( not string.EndsWith( f, ".lua" ) ) then
+            goto skipsceneload
+        end
+
+        local file = string.StripExtension( f )
+
+        local Btn = GUI.AddElement( "rectbutton", w * .05, ( h * .01 ) + #btns * ( h * .11 ), w * .9, h * .1 )
+        Btn:SetText( file )
+        Btn.file = file
+
+        Btn.Press = function( self )
+            _SCENETOLOAD = self.file
+            killbuttons()
+        end
+
+        table.insert( btns, Btn )
+
+        ::skipsceneload::
+    end
+end
+
+callback( "firstmenuframe", 
+  menucreate )
