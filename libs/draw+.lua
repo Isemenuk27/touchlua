7f21f6627735585cb5abe9652d1e6fa7b0a91128
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-01-19 13:39:32 +0200

    Update draw+.lua

--- libs/draw+.lua
+++ libs/draw+.lua
@@ -1,12 +1,55 @@
-local oline = draw.line
-local orect = draw.rect
-local otext = draw.text
-local ocirc = draw.circle
+local drawline = draw.line
+local drawrect = draw.rect
+local drawtext = draw.text
+local drawcirc = draw.circle
 local white = draw.white
 
+local oline, orect, otext, ocirc
+
 local type, TABLE = type, "table"
+
 local function istable(t)
     return type(t) == TABLE
+end
+
+if ( draw.getmatrix ) then
+    oline = function( a, b, c, d, col )
+        for i = draw.matstackl, 1, -1 do
+            local m = draw.getmatrix( i )
+            a, b = mat3mulxy( m, a, b )
+            c, d = mat3mulxy( m, c, d )
+        end
+
+        drawline( a, b, c, d, col )
+    end
+
+    orect = function( a, b, c, d, col )
+        for i = draw.matstackl, 1, -1 do
+            local m = draw.getmatrix( i )
+            a, b = mat3mulxy( m, a, b )
+            c, d = mat3mulxy( m, c, d )
+        end
+
+        drawrect( a, b, c, d, col )
+    end
+
+    otext = function( t, a, b, col )
+        for i = draw.matstackl, 1, -1 do
+            a, b = mat3mulxy( draw.getmatrix( i ), a, b )
+        end
+
+        drawtext( t, a, b, col )
+    end
+
+    ocirc = function( a, b, r, col )
+        for i = draw.matstackl, 1, -1 do
+            a, b = mat3mulxy( draw.getmatrix( i ), a, b )
+        end
+
+        drawcirc( a, b, r, col )
+    end
+else
+    oline, orect, otext, ocirc = draw.line, draw.rect, draw.text, draw.circle
 end
 
 local function line( x1, y1, x2, y2, col )
@@ -26,7 +69,7 @@ end
 
 local function text( str, ox, oy, col )
     if ( istable( ox ) ) then
-        ox, oy, col = ox[1], oy[2], oy
+        ox, oy, col = ox[1], ox[2], oy
     end
     return otext( str, ox, oy, col or white )
 end
@@ -42,6 +85,5 @@ do
     draw.line = line
     draw.cross = cross
     draw.text = text
-    draw.circle = circ
-	le
+    draw.circle = circle
 end
