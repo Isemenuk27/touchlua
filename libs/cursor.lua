91494e2270232cb54ad1b7c6e24798dbc5767724
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-01-22 10:16:00 +0200

    Update cursor.lua

--- libs/cursor.lua
+++ libs/cursor.lua
@@ -2,11 +2,13 @@ assert( callback, "missing callback lib" )
 
 local exec = exec
 local mx, my, mh = 0, 0, false
+local dx, dy = 0, 0
 
 function draw.touchbegan( t )
     exec( "touch.start", t )
 
     if ( t.id == 1 ) then
+        dx, dy = 0, 0
         mx, my = t.x, t.y
         mh = true
     end
@@ -16,6 +18,7 @@ function draw.touchmoved( t )
     exec( "touch.move", t )
 
     if ( t.id == 1 ) then
+        dx, dy = t.x - mx, t.y - my
         mx, my = t.x, t.y
     end
 end
@@ -24,6 +27,7 @@ function draw.touchended( t )
     exec( "touch.end", t )
 
     if ( t.id == 1 ) then
+        dx, dy = t.x - mx, t.y - my
         mx, my = t.x, t.y
         mh = false
     end
@@ -33,7 +37,27 @@ function cursor()
     return mx, my
 end
 
+function cursorv()
+    return vec2( mx, my )
+end
+
 function cursordown()
-    retu
-rn mh
+    return mh
+end
+
+function cursordeltax()
+    return dx
+end
+
+function cursordeltay()
+    return dy
+end
+
+function cursordelta()
+    return vec2( dx, dy )
+end
+
+function cursorcleardelta()
+    dx = 0
+    dy = 0
 end
