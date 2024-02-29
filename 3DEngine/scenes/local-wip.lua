d9d3a23482de16d86e71c619d017d2daa37935b9
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-02-29 19:08:27 +0200

    Update local-wip.lua

--- 3DEngine/scenes/local-wip.lua
+++ 3DEngine/scenes/local-wip.lua
@@ -1,11 +1,20 @@
+if ( not Inited ) then
+    local path = debug.getinfo(1, "S").source:match("@(.*)")
+    for i = #path, 1, -1 do; local c = string.sub( path, i, i );
+    if ( c == "/" or c == "\\" ) then return string.sub( path, i + 1 ) end end
+    _SCENETOLOAD = string.sub( path, 1, -5)
+    package.path = package.path .. ";../../?.lua"
+    return require("3DEngine/init")
+end
+
 local obj = createclass( C_POLY )
 obj:born()
-obj.form = loadobj( "axis.obj" )
+obj.form = loadModel( "axis.mdl" )
 obj.scl = vec3( 1/10 )
 
 local obj2 = createclass( C_POLY )
 obj2:born()
-obj2.form = loadobj( "cube.obj" )
+obj2.form = loadModel( "cube.mdl" )
 obj2.scl = vec3( 1/3 )
 obj2.ang = vec3( 0, 0, 0 )
 
@@ -16,8 +25,10 @@ obj2.ang = vec3( 0, 0, 0 )
 local function Loop( CT, DT )
     vec3set( obj.ang, 0, ( CT * .2 * math.pi ) % ( math.pi * 2 ) , 0 )
 
-    local pos, ang = mat4localtoworld( obj.pos, obj.ang, vec3( 1, 0, 0 ), vec3( 0, 0, math.pi * .25 ) )
+    local pos, ang = mat4localtoworld( obj.pos, obj.ang, vec3( 2, 0, 0 ), vec3( 0, 0, 0 ) )
+    table.shiftRight( ang, 1 )
     vec3set( obj2.pos, pos )
+    vec3mul( ang, -1 )
     vec3set( obj2.ang, ang )
 end
 
