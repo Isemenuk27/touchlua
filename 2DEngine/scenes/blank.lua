e3767687d605a2d955b0fab89f3cb33cf4ffc0b7
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-02-29 19:10:48 +0200

    Update scenes

---
+++ 2DEngine/scenes/blank.lua
@@ -0,0 +1,19 @@
+if ( not Inited ) then
+    local path = debug.getinfo(1, "S").source:match("@(.*)")
+    for i = #path, 1, -1 do; local c = string.sub( path, i, i );
+    if ( c == "/" or c == "\\" ) then return string.sub( path, i + 1 ) end end
+    _SCENETOLOAD = string.sub( path, 1, -5)
+    package.path = package.path .. ";../../?.lua"
+    return require("2DEngine/init")
+end
+
+local function Init()
+
+end
+
+local function Loop( CT, DT )
+
+end
+
+callback( _LOOPCALLBACK, Loop )
+callback( "Init", Init )
