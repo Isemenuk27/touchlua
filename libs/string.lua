dd8cab181b4504885ffe6dc58b0137909bd50134
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-04-03 10:32:12 +0300

    Update string.lua

--- libs/string.lua
+++ libs/string.lua
@@ -18,23 +18,10 @@ function string.Explode( sSeparator, sInput )
     end
 
     return tRes
+end
 
-    --[[    if ( separator == "" ) then return totable( str ) end
-    if ( withpattern == nil ) then withpattern = false end
-
-    local ret = {}
-    local current_pos = 1
-
-    for i = 1, #str do
-        local start_pos, end_pos = string_find( str, separator, current_pos, not withpattern )
-        if ( not start_pos ) then break end
-        ret[ i ] = string_sub( str, current_pos, start_pos - 1 )
-        current_pos = end_pos + 1
-    end
-
-    ret[ #ret + 1 ] = string_sub( str, current_pos )
-
-    return ret]]--
+function printf( sFormat, ... )
+    return print( string.format( sFormat, ... ) )
 end
 
 function string.Split( str, delimiter )
@@ -138,4 +125,10 @@ function string.Comma( number, str )
 
     return number
 
+end
+
+local sFormat = "[^\n]+"
+
+function string.lines( sStr )
+    return string.gmatch( sStr, sFormat )
 end
