6367081190cf3d64618c760bfa412ca6b366fbe6
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-04-11 14:50:21 +0300

    Update cursor+.lua

--- libs/cursor+.lua
+++ libs/cursor+.lua
@@ -1,8 +1,6 @@
---assert( callback, "missing callback lib" )
---assert( vec2, "vec2 lib is missing" )
-
 cursor = {
     tActive = {},
+    cFakeCursor = false,
     tCallbacks = {
         began = "cursor.began",
         moved = "cursor.moved",
@@ -10,184 +8,171 @@ cursor = {
     }
 }
 
-local function createCursor( nTId )
+function cursor.create( nTId )
     return {
-        pos = vec2(),
-        prevpos = vec2(),
-        delta = vec2(),
-        down = false,
-        id = nTId,
-        downtime = -1,
-        pressframe = -1,
+        vPos = vec2(),
+        vPrevPos = vec2(),
+        vDelta = vec2(),
+        nDowntime = 0,
+        nPressframe = 0,
+        bDown = false,
+        nTId = nTId,
     }
 end
 
-local cCursorFallback = createCursor(1)
+cursor.cFakeCursor = cursor.create( 0 )
 
-local function getCursor( nId )
-    return cursor.tActive[nId or 1]
-end
-
-local function getCursorList()
-    return cursor.tActive
-end
-
-local function pushCursor( cCursor )
-    local nL = #getCursorList() + 1
-    getCursorList()[nL] = cCursor
-    return nL
-end
-
-local function getCursorIndex( cCursor )
-    for j, c in ipairs( getCursorList() ) do
-        if ( c == cCursor ) then return j end
-    end
-    return false
-end
-
-local function getCursorIndexByTId( nTId )
-    for j, c in ipairs( getCursorList() ) do
-        if ( c.id == nTId ) then return j end
-    end
-    return 0
+function cursor.getFakeCursor()
+    return cursor.cFakeCursor
 end
 
 local function getCursorByTId( nTId )
-    return getCursor( getCursorIndexByTId( nTId ) )
+    for _, cCursor in ipairs( cursor.tActive ) do
+        if ( cCursor.nTId == nTId ) then
+            return cCursor
+        end
+    end
+end
+
+local function pushCursor( cCursor )
+    return table.insert( cursor.tActive, cCursor )
 end
 
 local function removeCursor( cCursor )
-    table.remove( getCursorList(), getCursorIndex( cCursor ) )
+    for nId, cFoundCursor in ipairs( cursor.tActive ) do
+        if ( cCursor == cFoundCursor ) then
+            return table.remove( cursor.tActive, nId )
+        end
+    end
 end
+
+cursor.removeCursor = removeCursor
+cursor.pushCursor = pushCursor
+cursor.getCursorByTId = getCursorByTId
+
+local function handleCursor( cCursor, nX, nY )
+    vec2set( cCursor.vPrevPos, cCursor.vPos )
+    vec2set( cCursor.vPos, nX, nY )
+    vec2diff( cCursor.vPrevPos, cCursor.vPos, cCursor.vDelta )
+end
+
+local function handleCursorPress( cCursor )
+    cCursor.nPressframe = frameNum()
+    cCursor.nDowntime = curtime()
+    cCursor.bDown = true
+end
+
+local function handleCursorRelease( cCursor )
+    cCursor.bDown = false
+end
+
+local function cursorBegan( t )
+    local nTId, nX, nY = t.id, t.x, t.y
+    local cCursor = cursor.create( nTId )
+
+    handleCursor( cCursor, nX, nY )
+    handleCursorPress( cCursor )
+    vec2set( cCursor.vDelta, 0, 0 )
+
+    if ( nTId == 1 ) then
+        handleCursor( cursor.cFakeCursor, nX, nY )
+        handleCursorPress( cursor.cFakeCursor )
+        vec2set( cursor.cFakeCursor.vDelta, 0, 0 )
+    end
+
+    pushCursor( cCursor )
+
+    exec( cursor.tCallbacks.began, cCursor )
+end
+
+local function cursorMoved( t )
+    local nTId, nX, nY = t.id, t.x, t.y
+    local cCursor = getCursorByTId( nTId )
+
+    handleCursor( cCursor, nX, nY )
+
+    if ( nTId == 1 ) then
+        handleCursor( cursor.cFakeCursor, nX, nY )
+    end
+
+    exec( cursor.tCallbacks.moved, cCursor )
+end
+
+local function cursorEnded( t )
+    local nTId, nX, nY = t.id, t.x, t.y
+    local cCursor = getCursorByTId( nTId )
+
+    handleCursor( cCursor, nX, nY )
+    handleCursorRelease( cCursor )
+
+    if ( nTId == 1 ) then
+        handleCursor( cursor.cFakeCursor, nX, nY )
+        handleCursorRelease( cursor.cFakeCursor )
+    end
+
+    exec( cursor.tCallbacks.ended, cCursor )
+
+    removeCursor( cCursor )
+end
+
+draw.touchbegan = cursorBegan
+draw.touchmoved = cursorMoved
+draw.touchended = cursorEnded
 
 --********************
 
-function cursor.pos( nId )
-    return getCursor( nId ).pos
+function cursor.pos( cCursor )
+    return cCursor and cCursor.vPos or cursor.cFakeCursor.vPos
 end
 
-function cursor.pos2( nId )
-    return vec2unpack( getCursor( nId ).pos )
+function cursor.pos2( cCursor )
+    return vec2unpack( cursor.pos( cCursor ) )
 end
 
 if ( mat3 ) then
     local mTemp = mat3()
     local vOut = vec2()
 
-    function cursor.pos3( nId )
+    function cursor.pos3( cCursor )
         local mCursor = mat3()
         for i = draw.matstackl, 1, -1 do
             local m = draw.getmatrix( i )
             mat3mul( mCursor, mat3inv( m, mTemp ), mCursor )
         end
 
-        return vec2set( vOut, mat3mulxy( mCursor, cursor.pos2( nId ) ) )
+        return vec2set( vOut, mat3mulxy( mCursor, cursor.pos2( cCursor ) ) )
     end
 end
 
-function cursor.isPressed( nId )
-    return getCursor( nId ).down
+function cursor.isPressed( cCursor )
+    return cCursor and cCursor.bDown or false
 end
 
-function cursor.delta( nId )
-    return getCursor( nId ).delta
+function cursor.delta( cCursor )
+    return cCursor and cCursor.vDelta or cursor.cFakeCursor.vDelta
 end
 
-function cursor.delta2( nId )
-    return vec2unpack( getCursor( nId ).delta )
+function cursor.delta2( cCursor )
+    return vec2unpack( cursor.delta( cCursor ) )
 end
 
-function cursor.isTapped( nId )
-    return getCursor( nId ).pressframe == frameNum()
+function cursor.isTapped( cCursor )
+    return cCursor and ( cCursor.nPressframe == frameNum() ) or false
 end
 
+function cursor.getPressDuration( cCursor )
+    return curtime() - cCursor.nDowntime
+end
+
+function cursor.clearDelta()
+    for i, cCursor in ipairs( cursor.tActive ) do
+        vec2set( cCursor.vDelta, 0, 0 )
+    end
+
+    vec2set( cursor.cFakeCursor.vDelta, 0, 0 )
+end
+
+local iterator = ipairs({})
 function cursor.down()
-    local nIndex = 0
-    return function()
-        while ( nIndex < #getCursorList() ) do
-            nIndex = nIndex + 1
-            if ( cursor.isPressed( nIndex ) ) then
-                return nIndex
-            end
-        end
-    end
+    return iterator, cursor.tActive, 0
 end
-
-function cursor.getPressDuration( nId )
-    return cursor.isPressed( nId ) and ( cursor.downtime( nId ) - curtime() ) or 0
-end
-
-function cursor.clearDelta( nId )
-    if ( #getCursorList() == 0 ) then return end
-
-    if ( nId ~= true ) then
-        vec2set( getCursor( nId ).delta, 0, 0 )
-        return true
-    end
-
-    for i, cCursor in ipairs( getCursorList() ) do
-        vec2set( cCursor.delta, 0, 0 )
-    end
-end
-
-cursor.getCursor = getCursor
-
---********************
-
-pushCursor( cCursorFallback )
-
-local function cursorBegan( t )
-    local nTId, nX, nY = t.id, t.x, t.y
-    local nId = getCursorIndexByTId( nTId )
-    local cCursor = getCursor( nId )
-
-    if ( not cCursor ) then
-        cCursor = createCursor( nTId )
-        nId = pushCursor( cCursor )
-    end
-
-    print( cCursor, nId, nTId, nX, nY )
-
-    exec( cursor.tCallbacks.began, nId )
-
-    vec2set( cCursor.prevpos, cCursor.pos )
-    vec2set( cCursor.pos, nX, nY )
-    vec2set( cCursor.delta, 0, 0 )
-    cCursor.down = true
-    cCursor.pressframe = frameNum()
-    cCursor.downtime = curtime()
-end
-
-local function cursorMoved( t )
-    local nTId, nX, nY = t.id, t.x, t.y
-    local nId = getCursorIndexByTId( nTId )
-    local cCursor = getCursor( nId )
-
-    if ( cCursor ) then
-        exec( cursor.tCallbacks.moved, nId )
-
-        vec2set( cCursor.prevpos, cCursor.pos )
-        vec2set( cCursor.pos, nX, nY )
-        vec2diff( cCursor.prevpos, cCursor.pos, cCursor.delta )
-    end
-end
-
-local function cursorEnded( t )
-    local nTId, nX, nY = t.id, t.x, t.y
-    local nId = getCursorIndexByTId( nTId )
-    local cCursor = getCursor( nId )
-
-    if ( cCursor ) then
-        exec( cursor.tCallbacks.ended, nId )
-
-        vec2set( cCursor.prevpos, cCursor.pos )
-        vec2set( cCursor.pos, nX, nY )
-        vec2diff( cCursor.prevpos, cCursor.pos, cCursor.delta )
-
-        cCursor.down = false
-    end
-end
-
-draw.touchbegan = cursorBegan
-draw.touchmoved = cursorMoved
-draw.touchended = cursorEnded
