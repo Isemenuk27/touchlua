cf6e119264e8a330442cbc7c8ff17d2a0a320afe
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-04-11 14:52:45 +0300

    Update GUI

---
+++ GUI/screen.lua
@@ -0,0 +1,45 @@
+if ( not bInitialized ) then require( "main" ) return end
+
+local nW, nH, nHW, nHH, ratio, ratio2
+
+function showscreen()
+    draw.showdrawscreen()
+    nW, nH = draw.getdrawsize()
+    nHW, nHH = nW  * .5, nH * .5
+    ratio  = nH / nW
+    ratio2 = nW / nH
+
+    exec( "ShowScreen" )
+end
+
+function ScrW( m )
+    return nW * ( m or 1 )
+end
+
+function ScrH( m )
+    return nH * ( m or 1 )
+end
+
+function HScrW( m )
+    return nHW * ( m or 1 )
+end
+
+function HScrH( m )
+    return nHH * ( m or 1 )
+end
+
+function Scr()
+    return nW, nH
+end
+
+function HScr()
+    return nHW, nHH
+end
+
+function ScrRatio()
+    return ratio
+end
+
+function ScrRatio2()
+    return ratio2
+end
