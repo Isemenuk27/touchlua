43023061eb62d1b42b3b82aa22a138925a5c6e70
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2024-05-22 12:09:21 +0300

    Update render.lua

--- 3D-Engine+/render.lua
+++ 3D-Engine+/render.lua
@@ -51,52 +51,39 @@ function render.getVertexIndexData( tModel )
 end
 
 --**************************************
+local nEPS = 1e-5
 
-local trianglePlane do
-    local vDiff = vec3()
+local function trianglePlane( nAX, nAY, nAZ, nBX, nBY, nBZ, nCX, nCY, nCZ, vPos, vDir )
+    local nX, nY, nZ = vPos[1], vPos[2], vPos[3]
+    local nNX, nNY, nNZ = vDir[1], vDir[2], vDir[3]
 
-    trianglePlane = function( tPoints, vPos, vDir )
-        local c = 0
+    return nEPS < ( nNX * ( nX - nAX ) + nNY * ( nY - nAY ) + nNZ * ( nZ - nAZ ) ),
+    nEPS < ( nNX * ( nX - nBX ) + nNY * ( nY - nBY ) + nNZ * ( nZ - nBZ ) ),
+    nEPS < ( nNX * ( nX - nCX ) + nNY * ( nY - nCY ) + nNZ * ( nZ - nCZ ) )
+end
 
-        for i = 1, 3 do
-            vec3diffto( tPoints[i], vPos, vDiff )
-            local dot = vec3dot( vDir, vDiff )
+local function planeLineIntersection( nOX, nOY, nOZ, nNX, nNY, nNZ, nX0, nY0, nZ0, nX1, nY1, nZ1 )
+    local nDX0, nDY0, nDZ0 = nX0 - nOX, nY0 - nOY, nZ0 - nOZ
+    local nDX1, nDY1, nDZ1 = nX1 - nOX, nY1 - nOY, nZ1 - nOZ
 
-            if ( dot > 0 ) then
-                c = c + 1
-            end
-        end
+    local nD1, nD2 = ( nDX0 * nNX ) + ( nDY0 * nNY ) + ( nDZ0 * nNZ ),
+    ( nDX1 * nNX ) + ( nDY1 * nNY ) + ( nDZ1 * nNZ )
 
-        return ( c == 3 and -1 ) or ( c > 0 and 1 ) or 0
-    end
+    local nT = nD1 / ( nD2 - nD1 )
+
+    return nX0 - ( nX1 - nX0 ) * nT, nY0 - ( nY1 - nY0 ) * nT, nZ0 - ( nZ1 - nZ0 ) * nT, -nT
 end
 
 local toScreen do
     local vOut = vec3()
 
-    toScreen = function( vPoint, mProj, nHW, nHH, nH )
+    toScreen = function( vPoint, mProj )
         mat4mulVector( mProj, vPoint, vOut )
 
         vec3add( vOut, 1, 1, 0 )
-        vec3mul( vOut, nHW, nHH, 1 )
+        vec3mul( vOut, HScrW(), HScrH(), 1 )
 
-        return vOut[1], nH - vOut[2], vOut[3]
-    end
-end
-
-local toScreenC do
-    local function mat4mulVectorC( m, nVx, nVy, nVz )
-        local nR0, nR1, nR2, nR3 = m[0], m[1], m[2], m[3]
-        local nW = 1 / ( nVx * nR0[3] + nVy * nR1[3] + nVz * nR2[3] + nR3[3] )
-
-        return ( nVx * nR0[0] + nVy * nR1[0] + nVz * nR2[0] + nR3[0] ) * nW,
-        ( nVx * nR0[1] + nVy * nR1[1] + nVz * nR2[1] + nR3[1] ) * nW,
-        ( nVx * nR0[2] + nVy * nR1[2] + nVz * nR2[2] + nR3[2] ) * nW
-    end
-
-    toScreenC = function( nX, nY, nZ, mProj, nHW, nHH, nH )
-        nX, nY, nZ = mat4mulVectorC( mProj, nX, nY, nZ )
-        return ( nX + 1 ) * nHW, nH - ( nY + 1 ) * nHH, nZ
+        return vOut[1], ScrH() - vOut[2], vOut[3]
     end
 end
 
@@ -117,7 +104,11 @@ end
 
 function render.init()
     local tCam = cam.new()
-    cam.buildPerspective( tCam, math.deg( 65 ), ScrW(), ScrH(), .1, 10 )
+    cam.buildPerspective( tCam,
+    math.pi * .5, ScrW(), ScrH(),
+    .1, 10 )
+
+    cam.setResolution( tCam, 2 ^ 7 )
     render.pushCamera( tCam )
 end
 
@@ -132,85 +123,224 @@ local mRotationY, mRotationX, mRotationZ = mat4(), mat4(), mat4()
 local mView, mViewProjection = mat4(), mat4()
 local vFaceNormal, vDirToFace = vec3(), vec3()
 
-function render.view( tCam, nTime, nFrameTime )
-    local mProj = cam.matrix( tCam )
-    local vCamPos = tCam.vPos
+do
+    local filltriangle = draw.filltriangle
+    local nHScrW, nHScrH, nScrH
+    local vCamPos, mProj
 
-    mat4identity( mView ) -- Reset marix
+    local function mat4mulVectorC( m, nVx, nVy, nVz, nU, nV )
+        local nR0, nR1, nR2, nR3 = m[0], m[1], m[2], m[3]
+        local nW = 1 / ( nVx * nR0[3] + nVy * nR1[3] + nVz * nR2[3] + nR3[3] )
 
-    local nPitch, nYaw, nRoll = vec3unpack( tCam.vAng )
+        return ( nVx * nR0[0] + nVy * nR1[0] + nVz * nR2[0] + nR3[0] ) * nW,
+        ( nVx * nR0[1] + nVy * nR1[1] + nVz * nR2[1] + nR3[1] ) * nW,
+        ( nVx * nR0[2] + nVy * nR1[2] + nVz * nR2[2] + nR3[2] ) * nW,
+        nU, nV, nW
+    end
 
-    -- Setup rotations
-    mat4rotateX( mRotationX, nPitch )
-    mat4rotateY( mRotationY, nYaw )
-    mat4rotateZ( mRotationZ, nRoll )
+    local function toScreenC( nX, nY, nZ, nU, nV )
+        local nOX, nOY, nOZ, nOU, nOV, nOW = mat4mulVectorC( mViewProjection, nX, nY, nZ, nU, nV )
+        return ( nOX + 1 ) * nHScrW, nScrH - ( nOY + 1 ) * nHScrH, nOZ,
+        nOU * nOW, nOV * nOW, nOW
+    end
 
-    -- Apply the to matrix
-    mat4mul( mView, mRotationY, mView )
-    mat4mul( mView, mRotationX, mView )
-    mat4mul( mView, mRotationZ, mView )
 
-    -- Should reset firstly
-    vec3setc( tCam.vDir, 0, 0, 1 )
-    vec3setc( tCam.vUpDir, 0, 1, 0 )
+    local vColor = vec3( 1, 1, 1 )
+    vColor[4] = 1
 
-    -- Apply rotations to vector
-    mat4mulVector( mView, tCam.vDir, tCam.vDir )
+    local tTriangleList = {}
 
-    local _, vUp, vForward, vRight = mat4lookAt( mView, vCamPos, tCam.vDir, tCam.vUpDir, tCam.vForwardDir, tCam.vRightDir )
+    local function pushTriangle( nAX, nAY, nAZ, nBX, nBY, nBZ, nCX, nCY, nCZ, nNX, nNY, nNZ, nAU, nAV, nBU, nBV, nCU, nCV )
+        local nTriangleListLen = #tTriangleList
 
-    mat4quickInverse( mView, mViewProjection )
-    mat4mul( tCam.mProj, mViewProjection, mViewProjection )
-    -- Now its time to project points
+        tTriangleList[nTriangleListLen+1], tTriangleList[nTriangleListLen+2], tTriangleList[nTriangleListLen+3],
+        tTriangleList[nTriangleListLen+4], tTriangleList[nTriangleListLen+5], tTriangleList[nTriangleListLen+6],
+        tTriangleList[nTriangleListLen+7], tTriangleList[nTriangleListLen+8], tTriangleList[nTriangleListLen+9],
+        tTriangleList[nTriangleListLen+10], tTriangleList[nTriangleListLen+11], tTriangleList[nTriangleListLen+12],
+        tTriangleList[nTriangleListLen+13], tTriangleList[nTriangleListLen+14],
+        tTriangleList[nTriangleListLen+15], tTriangleList[nTriangleListLen+16],
+        tTriangleList[nTriangleListLen+17], tTriangleList[nTriangleListLen+18] = nAX, nAY, nAZ, nBX, nBY, nBZ, nCX, nCY, nCZ, nNX, nNY, nNZ, nAU, nAV, nBU, nBV, nCU, nCV
+    end
 
-    local nHScrW, nHScrH = tCam.nWidth * .5, tCam.nHeight * .5
-    local nScrH = tCam.nHeight
+    function render.view( tCam, nTime, nFrameTime )
+        mProj, vCamPos = cam.matrix( tCam ), tCam.vPos
 
-    local tModel = mdl.get( "cube.mdl" )
-    draw.setTexture( render.getTexture( "cat.bmp" ) )
+        draw.initTextured( tCam.nWidth, tCam.nHeight, tCam.nVW )
 
-    for nI = tModel.nFaceArrayOffset, tModel.nFaceArrayOffset + tModel.nFaceArrayLen, 6 do
-        local nVertexA, nVertexB, nVertexC = tModel[nI], tModel[nI+1], tModel[nI+2]
+        mat4identity( mView ) -- Reset marix
 
-        local nAX, nAY, nAZ, nBX, nBY, nBZ, nCX, nCY, nCZ =
-        tModel[nVertexA], tModel[nVertexA+1], tModel[nVertexA+2],
-        tModel[nVertexB], tModel[nVertexB+1], tModel[nVertexB+2],
-        tModel[nVertexC], tModel[nVertexC+1], tModel[nVertexC+2];
+        local nPitch, nYaw, nRoll = vec3unpack( tCam.vAng )
 
-        local nNX, nNY, nNZ = tModel[nI+3], tModel[nI+4], tModel[nI+5];
+        -- Setup rotations
+        mat4rotateX( mRotationX, nPitch )
+        mat4rotateY( mRotationY, nYaw )
+        mat4rotateZ( mRotationZ, nRoll )
 
-        vec3setc( vFaceNormal, nNX, nNY, nNZ )
-        vec3setv( vDirToFace, vCamPos )
-        vec3subc( vDirToFace, nAX, nAY, nAZ )
+        -- Apply the to matrix
+        mat4mul( mView, mRotationY, mView )
+        mat4mul( mView, mRotationX, mView )
+        mat4mul( mView, mRotationZ, mView )
 
-        if ( vec3dot( vDirToFace, vFaceNormal ) < 0 ) then
-            goto skipface
+        -- Should reset firstly
+        vec3setc( tCam.vDir, 0, 0, 1 )
+        vec3setc( tCam.vUpDir, 0, 1, 0 )
+
+        -- Apply rotations to vector
+        mat4mulVector( mView, tCam.vDir, tCam.vDir )
+
+        local _, vUp, vForward, vRight = mat4lookAt( mView, vCamPos, tCam.vDir, tCam.vUpDir, tCam.vForwardDir, tCam.vRightDir )
+
+        mat4quickInverse( mView, mViewProjection )
+        mat4mul( tCam.mProj, mViewProjection, mViewProjection )
+        -- Now its time to project points
+
+        cam.updateFrustum( tCam )
+        local tClipPlanes = tCam.tFrustum.tPlanes
+
+        nHScrW, nHScrH, nScrH = tCam.nWidth * .5, tCam.nHeight * .5, tCam.nHeight
+
+        local tModel = mdl.get( "cube.mdl" )
+        draw.setTexture( render.getTexture(
+        "cat.bmp" ) )
+
+        tTriangleList = {}
+
+        for nI = tModel.nFaceArrayOffset, tModel.nFaceArrayOffset + tModel.nFaceArrayLen, 6 do
+            local nVertexA, nVertexB, nVertexC = tModel[nI], tModel[nI+1], tModel[nI+2]
+
+            -- Point of 3 vertecies
+            local nAX, nAY, nAZ, nBX, nBY, nBZ, nCX, nCY, nCZ =
+            tModel[nVertexA], tModel[nVertexA+1], tModel[nVertexA+2],
+            tModel[nVertexB], tModel[nVertexB+1], tModel[nVertexB+2],
+            tModel[nVertexC], tModel[nVertexC+1], tModel[nVertexC+2]
+
+            -- Normals is precomputed
+            local nNX, nNY, nNZ = tModel[nI+3], tModel[nI+4], tModel[nI+5]
+            vec3setc( vFaceNormal, nNX, nNY, nNZ )
+
+            vec3setv( vDirToFace, vCamPos ) -- Direction to camera
+            vec3subc( vDirToFace, nAX, nAY, nAZ )
+
+            -- Backface culling
+            if ( vec3dot( vDirToFace, vFaceNormal ) < 0 ) then
+                goto skip
+            end
+
+            pushTriangle( nAX, nAY, nAZ,
+            nBX, nBY, nBZ,
+            nCX, nCY, nCZ,
+            nNX, nNY, nNZ,
+            tModel[nVertexA+3], tModel[nVertexA+4],
+            tModel[nVertexB+3], tModel[nVertexB+4],
+            tModel[nVertexC+3], tModel[nVertexC+4] )
+
+            ::skip::
         end
 
-        local nAU, nAV, nBU, nBV, nCU, nCV =
-        tModel[nVertexA+3], tModel[nVertexA+4],
-        tModel[nVertexB+3], tModel[nVertexB+4],
-        tModel[nVertexC+3], tModel[nVertexC+4]
+        local nI = 1 - 18
 
-        local nX1, nY1, nZ1 = toScreenC( nAX, nAY, nAZ, mViewProjection, nHScrW, nHScrH, nScrH )
-        local nX2, nY2, nZ2 = toScreenC( nBX, nBY, nBZ, mViewProjection, nHScrW, nHScrH, nScrH )
-        local nX3, nY3, nZ3 = toScreenC( nCX, nCY, nCZ, mViewProjection, nHScrW, nHScrH, nScrH )
+        while ( true ) do
+            nI = nI + 18
 
-        --draw.triangle( nX1, nY1, nX2, nY2, nX3, nY3, draw.black )
-        --math.randomseed( nI )
+            if ( not tTriangleList[nI] ) then
+                break
+            end
 
-        draw.texturedTriangle( nX1, nY1, nX2, nY2, nX3, nY3,
-        nAU, nAV, nBU, nBV, nCU, nCV )
-        --draw.filltriangle( nX1, nY1, nX2, nY2, nX3, nY3, { math.random( 0, 10 ) * .1, math.random( 0, 10 ) * .1, math.random( 0, 10 ) * .1, 1 } )
+            local nAX, nAY, nAZ, nBX, nBY, nBZ, nCX, nCY, nCZ,
+            nNX, nNY, nNZ, nAU, nAV, nBU, nBV, nCU, nCV = table.unpack( tTriangleList, nI, nI + 17 )
 
-        ::skipface::
-    end
-end
+            for nI = 1, #tClipPlanes, 2 do
+                -- Number of points in front of plane
+                local vOrigin, vNormal = tClipPlanes[nI], tClipPlanes[nI+1]
+                local bA, bB, bC = trianglePlane( nAX, nAY, nAZ, nBX, nBY, nBZ, nCX, nCY, nCZ, vOrigin, vNormal )
+
+                if ( bA and bB and bC ) then
+                    goto skip -- Triangle fully behind the plane, skip it
+                elseif ( not ( bA or bB or bC ) ) then
+                    --Triangle in bounds, skip any cliping
+                else
+                    local nC = ( bA and 1 or 0 ) + ( bB and 1 or 0 ) + ( bC and 1 or 0 )
+                    local nOX, nOY, nOZ, nNX, nNY, nNZ = vOrigin[1], vOrigin[2], vOrigin[3], vNormal[1], vNormal[2], vNormal[3]
+
+                    local nXS, nYS, nZS, nXE0, nYE0, nZE0, nXE1, nYE1, nZE1,
+                    nU0, nV0, nU1, nV1, nU2, nV2
+
+                    local bClip2 = nC == 1
+
+                    if ( bA == bClip2 ) then
+                        nXS, nYS, nZS = nAX, nAY, nAZ
+                        nU0, nV0, nU1, nV1, nU2, nV2 =
+                        nAU, nAV, nBU, nBV, nCU, nCV
+                        nXE0, nYE0, nZE0 = nBX, nBY, nBZ
+                        nXE1, nYE1, nZE1 = nCX, nCY, nCZ
+                    elseif ( bB == bClip2 ) then
+                        nXS, nYS, nZS = nBX, nBY, nBZ
+                        nU0, nV0, nU1, nV1, nU2, nV2 =
+                        nBU, nBV, nAU, nAV, nCU, nCV
+                        nXE0, nYE0, nZE0 = nAX, nAY, nAZ
+                        nXE1, nYE1, nZE1 = nCX, nCY, nCZ
+                    else
+                        nU0, nV0, nU1, nV1, nU2, nV2 =
+                        nCU, nCV, nAU, nAV, nBU, nBV
+                        nXS, nYS, nZS = nCX, nCY, nCZ
+                        nXE0, nYE0, nZE0 = nAX, nAY, nAZ
+                        nXE1, nYE1, nZE1 = nBX, nBY, nBZ
+                    end
+
+                    if ( bClip2 ) then
+                        local nX0, nY0, nZ0, nT0 = planeLineIntersection( nOX, nOY, nOZ, nNX, nNY, nNZ, nXS, nYS, nZS, nXE0, nYE0, nZE0 )
+                        local nX1, nY1, nZ1, nT1 = planeLineIntersection( nOX, nOY, nOZ, nNX, nNY, nNZ, nXS, nYS, nZS, nXE1, nYE1, nZE1 )
+
+                        local nNU0, nNV0 = nU0 + ( nU1 - nU0 ) * nT0, nV0 + ( nV1 - nV0 ) * nT0
+                        local nNU1, nNV1 = nU0 + ( nU2 - nU0 ) * nT1, nV0 + ( nV2 - nV0 ) * nT1
+
+                        pushTriangle( nXE0, nYE0, nZE0, nXE1, nYE1, nZE1, nX0, nY0, nZ0, nNX, nNY, nNZ,
+                        nU1, nV1, nU2, nV2, nNU0, nNV0  )
+                        pushTriangle( nX1, nY1, nZ1, nXE1, nYE1, nZE1, nX0, nY0, nZ0, nNX, nNY, nNZ,
+                        nNU1, nNV1, nU2, nV2, nNU0, nNV0  )
+                    else
+                        local nX0, nY0, nZ0, nT0 = planeLineIntersection( nOX, nOY, nOZ, nNX, nNY, nNZ, nXS, nYS, nZS, nXE0, nYE0, nZE0 )
+                        local nX1, nY1, nZ1, nT1 = planeLineIntersection( nOX, nOY, nOZ, nNX, nNY, nNZ, nXS, nYS, nZS, nXE1, nYE1, nZE1 )
+
+                        local nNU0, nNV0 = nU0 + ( nU1 - nU0 ) * nT0, nV0 + ( nV1 - nV0 ) * nT0
+                        local nNU1, nNV1 = nU0 + ( nU2 - nU0 ) * nT1, nV0 + ( nV2 - nV0 ) * nT1
+
+                        pushTriangle( nXS, nYS, nZS, nX0, nY0, nZ0, nX1, nY1, nZ1, nNX, nNY, nNZ,
+                        nU0, nV0, nNU0, nNV0, nNU1, nNV1 )
+                    end
+
+                    goto skip
+                end -- if end
+            end -- clip loop end
+
+            do
+                --Crazy shit
+                local nX1, nY1, nZ1, nU1, nV1, nW1 = toScreenC( nAX, nAY, nAZ, nAU, nAV )
+                local nX2, nY2, nZ2, nU2, nV2, nW2 = toScreenC( nBX, nBY, nBZ, nBU, nBV )
+                local nX3, nY3, nZ3, nU3, nV3, nW3 = toScreenC( nCX, nCY, nCZ, nCU, nCV )
+
+                --[[ ]]
+                draw.texturedTriangle(
+                nX1, nY1, nU1, nV1, nW1,
+                nX2, nY2, nU2, nV2, nW2,
+                nX3, nY3, nU3, nV3, nW3 ) --]]
+
+                --math.randomseed( nI )
+                --vec3setc( vColor, rand( 0, 1 ), rand( 0, 1 ), rand( 0, 1 ) )
+                --filltriangle( nX1, nY1, nX2, nY2, nX3, nY3, vColor )
+            end
+
+            ::skip::
+        end -- while end
+    end -- function end
+end -- do end
 
 function render.draw( nTime, nFrameTime )
+    local nT = sys.gettime()
     for _, tCam in ipairs( tCameraList ) do
         render.view( tCam, nTime, nFrameTime )
     end
+    draw.text( string.format( "%.04f", sys.gettime() - nT ), 60, 70 )
 end
 
 --**************************************
